575987ca67816d7e584adf73f38817376410f233
Yasar <yasarmansuri16@gmail.com>
2025-03-11 11:13:20 +0530

    Fetched Thumbnails

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import YOUR_API_KEY from "./API_CONSTANTS.js";
 
 const videoContainer = document.querySelector(".content");
 const video_https = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_https = "https://www.googleapis.com/youtube/v3/channels?";
 
 const numberOfVideosInInitialLoad = 20;
 const generateQueryParam = new URLSearchParams({
@@ -15,41 +16,38 @@ const generateQueryParam = new URLSearchParams({
 })
 
 // console.log(video_https + generateQueryParam);
-
-// video element
-function createVideoElement(video){
-    const videoElement = document.createElement("div");
-    videoElement.classList("video");
-
-    const videoThumbnail = document.createElement("img");
-    videoThumbnail.src = video.snippet.thumbnails.medium.url;
-
-    const videoTitle = document.createElement("h2");
-    videoTitle.textContent = video.snippet.title;
-
-    const channelTitle = document.createElement("p");
-    channelTitle.textContent = video.snippet.channelTitle;
-
-    videoElement.appendChild(videoThumbnail);
-    videoElement.appendChild(videoTitle);
-    videoElement.appendChild(channelTitle);
-
-    return videoElement;
-}
-
-function displayVideos(videos){
-    videos.forEach((video) => {
-        const videoElement = createVideoElement(video);
-        videoContainer.appendChild(videoElement);
-    });
-}
-
-
 fetch(video_https + generateQueryParam)
     .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        displayVideos(data.items);
+    .then((data) =>{
+        console.log(data)
+        data.items.forEach((item) => {
+            getChannelIcon(item);
+        })
     })
     .catch((err) => console.log(err));
 
+const getChannelIcon = (video_data)=>{
+    fetch(channel_https + new URLSearchParams({
+        key : YOUR_API_KEY,
+        part : "snippet",
+        id : video_data.snippet.channelId,
+    })).then((res)=>res.json())
+        .then((data)=>{
+            console.log(data);
+            video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+            makeVideoCard(video_data)
+        })
+        .catch((err)=>console.log(err));
+} 
+
+const makeVideoCard = (data) =>{
+    const videoCard = document.createElement("div");
+    videoCard.classList.add("video");
+    videoCard.addEventListener("click",()=>{
+        window.location.href="";
+    })
+    videoCard.innerHTML = `
+        <img src="${data.snippet.thumbnails.default.url}" class="thumbnail" alt="">
+    `
+    videoContainer.appendChild(videoCard);
+} 
