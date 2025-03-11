d6c546ecaa6d0607a822d848040d3a7c908a7ae5
Yasar <yasarmansuri16@gmail.com>
2025-03-11 11:10:50 +0530

    Fetched Thumbnail data

--- script.js
+++ script.js
@@ -16,7 +16,40 @@ const generateQueryParam = new URLSearchParams({
 
 // console.log(video_https + generateQueryParam);
 
+// video element
+function createVideoElement(video){
+    const videoElement = document.createElement("div");
+    videoElement.classList("video");
+
+    const videoThumbnail = document.createElement("img");
+    videoThumbnail.src = video.snippet.thumbnails.medium.url;
+
+    const videoTitle = document.createElement("h2");
+    videoTitle.textContent = video.snippet.title;
+
+    const channelTitle = document.createElement("p");
+    channelTitle.textContent = video.snippet.channelTitle;
+
+    videoElement.appendChild(videoThumbnail);
+    videoElement.appendChild(videoTitle);
+    videoElement.appendChild(channelTitle);
+
+    return videoElement;
+}
+
+function displayVideos(videos){
+    videos.forEach((video) => {
+        const videoElement = createVideoElement(video);
+        videoContainer.appendChild(videoElement);
+    });
+}
+
+
 fetch(video_https + generateQueryParam)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        console.log(data);
+        displayVideos(data.items);
+    })
     .catch((err) => console.log(err));
+
