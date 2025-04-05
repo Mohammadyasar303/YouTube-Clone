6656b89a634cc35006159b565b5708b757c46ce5
Yasar <yasarmansuri16@gmail.com>
2025-04-05 19:38:31 +0530

    login..& signup using firebase

---
+++ src/index.js
@@ -0,0 +1,25 @@
+ const express = require('express');
+ const pasth = require('path');
+ const bcrypt = require('bcrypt');
+
+ const app = express();
+
+//  use EJS as the viw engine
+ app.set('view engine', 'ejs');
+//  static file
+app.use(express.static('public'));
+
+ app.get('/',(req, res)=>{
+    res.render('login');
+ });
+
+ app.get('/signup',(req, res)=>{
+    res.render('signup');
+ });
+
+
+
+ const port = 5000;
+ app.listen(port, () => {
+     console.log(`Server is running on port ${port}`);
+ })
