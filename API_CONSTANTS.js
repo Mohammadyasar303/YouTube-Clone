8714fd245162f3c23a75664695090c085e55afbb
Yasar <yasarmansuri16@gmail.com>
2025-04-09 17:52:51 +0530

    .

--- API_CONSTANTS.js
+++ API_CONSTANTS.js
@@ -1,5 +1,6 @@
-export const YOUR_API_KEY = "AIzaSyCi25KWL-qHjkClxdCQJn_hHpL7346iCfc";// api key 1
-export const YOUR_API_SEARCH = "AIzaSyBUQX8j9i2bDwi2DYIfdoKXQduGuPKM7XI"; // api key 2
+export const YOUR_API_KEY = "AIzaSyDlPY7bmSlbR6qUNt9dC1yEVpHhTd6jolE";// api key 1
+export const YOUR_API_SEARCH = "AIzaSyCnmGh8moQFVLHf1wRyAiriX7stDLJs0xo"; // api key 2
+// export const FILTER_API_KEY = ""/ api key 5
 
 // export default YOUR_API_KEY;
 // export default videoPage_API;
